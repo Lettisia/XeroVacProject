d577c0f48d301659bda4c56dd8f0f862bd2ab8d2
Srushti Ruparel <srushti.ruparel@xero.com>
2017-12-05 10:02:48 +1100

    dbcontroller

---
+++ ReactApp/XeroProjectReact/Controllers/DbAccessController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using XeroVacProjectConsole.Models;
+
+namespace XeroProjectReact.Controllers
+{
+    public class DbAccessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private static NpgsqlConnection OpenDB()
+        {
+            var con = new NpgsqlConnection("Server=deathoftombstone.cfbtbnwur0gl.ap-southeast-2.rds.amazonaws.com;Port=5432;Database=DeathOfTombstone;User Id=Tombstone;Password=xerotomb");
+            con.Open();
+            return con;
+        }
+
+
+        public List<Location> InitialiseLocations()
+        {
+            var connection = OpenDB();
+            var command = new NpgsqlCommand("SELECT * FROM location", connection);
+            var reader = command.ExecuteReader();
+
+            List<Location> locations = new List<Location>();
+
+            while (reader.Read())
+            {
+
+                Location location = new Location();
+                location.id = reader.GetInt32(0);
+                location.name = reader.GetString(1);
+                location.east = CheckNullIntegers(reader, 2);
+                location.west = CheckNullIntegers(reader, 3);
+                location.north = CheckNullIntegers(reader, 4);
+                location.south = CheckNullIntegers(reader, 5);
+                location.description = reader.GetString(6);
+                location.verboseDescription = reader.GetString(7);
+                locations.Add(location);
+
+            }
+
+            Console.WriteLine(locations);
+
+            return locations;
+        }
+
+        public int? CheckNullIntegers(NpgsqlDataReader reader, int index)
+        {
+            if (reader.IsDBNull(index))
+                return null;
+            else
+                return reader.GetInt32(index);
+        }
+
+
+    }
+}
--- ReactApp/XeroProjectReact/Controllers/HomeController.cs
+++ ReactApp/XeroProjectReact/Controllers/HomeController.cs
@@ -24,12 +24,7 @@ namespace XeroProjectReact.Controllers
             return View();
         }
 
-        private static NpgsqlConnection OpenDB()
-        {
-            var con = new NpgsqlConnection("Server=deathoftombstone.cfbtbnwur0gl.ap-southeast-2.rds.amazonaws.com;Port=5432;Database=DeathOfTombstone;User Id=Tombstone;Password=xerotomb");
-            con.Open();
-            return con;
-        }
+       
 
         //https://stackoverflow.com/questions/44925223/how-to-pass-data-to-controller-using-fetch-api-in-asp-net-core to send objects
         [HttpPost]
@@ -40,28 +35,7 @@ namespace XeroProjectReact.Controllers
 
             return rows;
         }
-
-        [HttpPost]
-        public string DbAccessRow(string jsonStr)
-        {
-            Console.WriteLine(jsonStr);
-            var cmd = JsonConvert.DeserializeObject<Command>(jsonStr);
-            var connection = OpenDB();
-            var command = new NpgsqlCommand(cmd.DBQuery(), connection);
-            var reader = command.ExecuteReader();
-
-            var response = "";
-            if (reader.Read())
-            {
-                response = reader[0].ToString();
-            }
-
-            Console.WriteLine(response);
-            InitialiseLocations();
-
-            return response;
-        }
-
+       
 
         [HttpGet]
         public IEnumerable<string> TestDbCallDelete()
@@ -108,41 +82,6 @@ namespace XeroProjectReact.Controllers
 
         }
 
-        public List<Location> InitialiseLocations()
-        {
-            var connection = OpenDB();
-            var command = new NpgsqlCommand("SELECT * FROM location", connection);
-            var reader = command.ExecuteReader();
-
-            List<Location> locations = new List<Location>();
-            
-            while (reader.Read())
-            {
-
-                Location location = new Location();
-                location.id = reader.GetInt32(0);
-                location.name = reader.GetString(1);
-                location.east = CheckNullIntegers(reader,2);
-                location.west = CheckNullIntegers(reader, 3); 
-                location.north = CheckNullIntegers(reader, 4);
-                location.south = CheckNullIntegers(reader, 5);
-                location.description = reader.GetString(6);
-                location.verboseDescription = reader.GetString(7);
-                locations.Add(location);
-
-            }
-
-            Console.WriteLine(locations);
-
-            return locations;
-        }
-
-        public int? CheckNullIntegers(NpgsqlDataReader reader, int index)
-        {
-            if (reader.IsDBNull(index))
-                return null;
-            else
-                return reader.GetInt32(index);
-        }
+        
     }
 }
