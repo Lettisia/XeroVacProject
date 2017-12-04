e15685db5fdf0bc95894372e220386d903690263
Srushti Ruparel <srushti.ruparel@xero.com>
2017-12-05 09:45:41 +1100

    new models

---
+++ ReactApp/XeroProjectReact/Models/Character.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XeroProjectReact.Models
+{
+    public class Character
+    {
+        public int id { get; set; }
+
+        public string characterName { get; set; }
+
+        public string characterDescription { get; set; }
+
+        public int locationId { get; set; }
+
+        public string   CharacterQuery()
+        {
+
+            return $"SELECT * FROM character WHERE id = {id}";
+
+        }
+    }
+}
